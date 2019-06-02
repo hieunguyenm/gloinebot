173dcec8b7be79463657778544e899ee039c4b98
Hieu Nguyen <contact@nguyenhi.eu>
2019-06-02 17:18:18 +0100

    Fix bug with checking occupied rooms

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,8 +1,7 @@
 export const btoa = (str: string): string => Buffer.from(str).toString('base64');
 
-export const isOccupied = (aStart: number, bStart: number, bEnd: number): boolean =>
-  Math.abs(aStart - bStart) === 1 && Math.abs(aStart - bEnd) === 1 ||
-  Math.abs(aStart - bStart) === 0;
+export const isOccupied = (aStart: number, aEnd: number, bStart: number, bEnd: number): boolean =>
+  !(aEnd <= bStart || aStart >= bEnd);
 
 export const split = <T>(list: T[], size: number): T[][] =>
   list.reduce((acc, _, i, self) =>
